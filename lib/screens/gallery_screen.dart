16cb54b6bf00f472e95509e331c6a803f0f8998f
its-samarth <samarthkasma21@gmail.com>
2024-01-30 19:28:21 +0530

    single image responsive

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -9,52 +10,66 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
-  late List<String> images;
+  List<String>? images;
   late ScrollController scrollController;
 
   @override
   void initState() {
     super.initState();
     scrollController = ScrollController();
-    images = List.generate(16, (index) => 'https://picsum.photos/id/$index/800/1200'); // Higher resolution
+    images = List.generate(16, (index) => 'https://picsum.photos/id/$index/1200/1800');
     scrollController.addListener(() {
       if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
-        loadImages();
+        loadMoreImages();
       }
     });
   }
 
-  void loadImages() {
-    List<String> additionalImages = List.generate(16, (index) => 'https://picsum.photos/id/${images.length + index}/800/1200');
-    setState(() {
-      images.addAll(additionalImages);
-    });
+  void loadMoreImages() {
+    if (images != null) {
+      List<String> additionalImages = List.generate(
+        16,
+        (index) => 'https://picsum.photos/id/${images!.length + index}/1200/1800',
+      );
+      setState(() {
+        images!.addAll(additionalImages);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Image Gallery')),
-      body: StaggeredGridView.countBuilder(
+      body: StaggeredGridView.builder(
         controller: scrollController,
-        crossAxisCount: 4,
-        itemCount: images.length,
+        gridDelegate: SliverStaggeredGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Adjust as needed for full-screen display
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+          staggeredTileBuilder: (int index) => StaggeredTile.fit(2),
+        ),
+        itemCount: images?.length ?? 0,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FullScreenGallery(images: images, initialIndex: index),
-                ),
-              );
+              if (images != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FullScreenGallery(images: images!, initialIndex: index),
+                  ),
+                );
+              }
             },
-            child: Image.network(images[index], fit: BoxFit.cover),
+            child: CachedNetworkImage(
+              imageUrl: images?[index] ?? '',
+              fit: BoxFit.cover,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
           );
         },
-        staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
       ),
     );
   }
