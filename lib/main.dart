56f1bb2439a233ad715512187d7869399d170b7e
its-samarth <samarthkasma21@gmail.com>
2024-01-31 17:26:45 +0530

    18 mvp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/gallery_screen.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Beautiful Gallery App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      debugShowCheckedModeBanner: false, // Remove debug tag
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: GalleryScreen(),
+    );
+  }
+}
+
+class GalleryScreen extends StatefulWidget {
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  List<String> images = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  void _loadImages() {
+    List<String> additionalImages = List.generate(
+      100,
+      (index) => 'https://picsum.photos/id/${images.length + index}/800/1200',
+    );
+    setState(() {
+      images.addAll(additionalImages);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.brightness_4),
+            onPressed: () {
+              // Toggle between light and dark mode
+              if (Theme.of(context).brightness == Brightness.light) {
+                ThemeMode.dark;
+              } else {
+                ThemeMode.light;
+              }
+              setState(() {});
+            },
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+        ),
+        itemCount: images.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == images.length - 1) {
+            _loadImages();
+          }
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FullScreenGallery(images: images, initialIndex: index),
+                ),
+              );
+            },
+            child: CachedNetworkImage(
+              imageUrl: images[index],
+              fit: BoxFit.cover,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FullScreenGallery extends StatelessWidget {
+  final List<String> images;
+  final int initialIndex;
+
+  FullScreenGallery({required this.images, required this.initialIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PhotoViewGallery.builder(
+        itemCount: images.length,
+        builder: (context, index) {
+          return PhotoViewGalleryPageOptions(
+            imageProvider: NetworkImage(images[index]),
+            minScale: PhotoViewComputedScale.contained,
+            maxScale: PhotoViewComputedScale.covered * 2,
+          );
+        },
+        scrollPhysics: BouncingScrollPhysics(),
+        backgroundDecoration: BoxDecoration(
+          color: Colors.black,
+        ),
+        pageController: PageController(initialPage: initialIndex),
+      ),
     );
   }
 }
--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
@@ -10,59 +9,55 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
-  List<String>? images;
+  List<String> images = [];
   late ScrollController scrollController;
 
   @override
   void initState() {
     super.initState();
     scrollController = ScrollController();
-    images = List.generate(16, (index) => 'https://picsum.photos/id/$index/1200/1800');
+    _loadImages();
     scrollController.addListener(() {
       if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
-        loadMoreImages();
+        _loadImages();
       }
     });
   }
 
-  void loadMoreImages() {
-    if (images != null) {
-      List<String> additionalImages = List.generate(
-        16,
-        (index) => 'https://picsum.photos/id/${images!.length + index}/1200/1800',
-      );
-      setState(() {
-        images!.addAll(additionalImages);
-      });
-    }
+  void _loadImages() {
+    List<String> additionalImages = List.generate(
+      100,
+      (index) => 'https://picsum.photos/id/${images.length + index}/800/1200',
+    );
+    setState(() {
+      images.addAll(additionalImages);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Image Gallery')),
+      appBar: AppBar(title: Text(' Galery')),
       body: GridView.builder(
         controller: scrollController,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
+          crossAxisCount: 2,
           crossAxisSpacing: 4.0,
           mainAxisSpacing: 4.0,
         ),
-        itemCount: images?.length ?? 0,
+        itemCount: images.length,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
             onTap: () {
-              if (images != null) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FullScreenGallery(images: images!, initialIndex: index),
-                  ),
-                );
-              }
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FullScreenGallery(images: images, initialIndex: index),
+                ),
+              );
             },
             child: CachedNetworkImage(
-              imageUrl: images?[index] ?? '',
+              imageUrl: images[index],
               fit: BoxFit.cover,
               placeholder: (context, url) => CircularProgressIndicator(),
               errorWidget: (context, url, error) => Icon(Icons.error),
@@ -105,5 +100,6 @@ class FullScreenGallery extends StatelessWidget {
 void main() {
   runApp(MaterialApp(
     home: GalleryScreen(),
+    debugShowCheckedModeBanner: false,
   ));
 }
